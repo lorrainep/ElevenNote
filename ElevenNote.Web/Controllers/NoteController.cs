8e7628daf18280c8dfafc23dad1fd1e1b486e80b
Unknown <champ279@outlook.com>
2017-04-13 14:15:46 -0400

    Added delete functionality

--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -56,6 +56,75 @@ namespace ElevenNote.Web.Controllers
             return View(model);
         }
 
+        public ActionResult Edit(int id)
+        {
+            
+            var service = CreateNoteService();
+            var detail = service.GetNoteById(id);
+            var model =
+                new NoteEdit
+                {
+                    NoteId = detail.NoteId,
+                    Title = detail.Title,
+                    Content = detail.Content
+                };
+
+
+
+            return View();
+        }
+
+        [HttpPost] 
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if (model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateNoteService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note could not be updated.";
+                return RedirectToAction("Index");
+
+            }
+
+            ModelState.AddModelError("", "Your note could not be created.");
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+            
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeletePost(int id)
+        {
+            var service = CreateNoteService();
+
+            // TODO: Handle failure...wrap it in an if statement
+            service.DeleteNote(id);
+
+            TempData["SaveResult"] = "Your note was deleted!";
+
+            return RedirectToAction("Index");
+        }
+
+
+
         private NoteService CreateNoteService()
         {
             //can't do 
